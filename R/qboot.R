bdaf347dcaa91ce0f07f1878461c4303b4442301
marchtaylor <marchtaylor@gmail.com>
2019-03-15 18:28:55 +0100

    updated qboot documentation and functions

--- R/qboot.R
+++ R/qboot.R
@@ -1,5 +1,7 @@
 #' Calculate MSE quantile estimation error via bootstrapping and predict
-#' number of iterations needed for a given precision
+#' number of iterations needed for a given precision.
+#' 
+#' @description Use of relative standard error ('semu')
 #'
 #' @param X matrix. Statistic of interest across years (rows) and 
 #'   iterations (columns)
@@ -9,69 +11,81 @@
 #' @param niter vector. Cumulative number of iterations to perform bootstrapped
 #'   estimates (Default: \code{niter = exp(seq(log(ncol(X))*0.5, 
 #'   log(ncol(X)), length.out = 20))}).
+#' @param estfun function. Function name to be used in estimating 
+#'   bootstrapped quantile (Default: \code{estfun = "mean"}). 
 #' @param aggfun string. Name of function to apply to quantile relative error by year
 #'   for use in log-log model (Default: \code{aggfun = "max"}). Use of "max" is 
 #'   consistent with 'Prob3', which is the maximum annual probability of SSB 
 #'   dropping below Blim. 'Prob2' would be the average annual probability 
 #'   of SSB dropping below Blim (i.e. \code{aggfun = "mean"}).    
-#' @param targsemu numeric. Percent relative error (se/mu) desired.
 #' @param verbose logical. Should progress be printed 
 #'   (Default: code\{verbose = TRUE})
+#' @param rseTarget numeric. Desired relative standard error (as a percent) 
+#'   for which to predict the required number of iterations.
+#' @param ci numeric. Confidence interval percent to use for  
+#'   lower (\code{cilow}) and upper (\code{ciup}) limits of bootrapped 
+#'   quantiles. 
 #'
-#' @return list. Contains 
+#' @return list. Contains ...
 #' @export
 #'
 #' @examples
 #' load("data/SSB.Rdata")
+#' source("R/qboot.R")
+#' source("R/qbootplot.R")
 #' X <- array(ssb, dim = dim(ssb)[c(2,6)])
 #' dimnames(X) <- dimnames(ssb)[c(2,6)]
-#' res <- qboot(X, nboot = 10)
+#' res <- qboot(X, nboot = 10, rseTarget = 1)
+#' qbootplot(res)
 #' 
 #' 
 #' 
 qboot <- function(X, nboot = 20, quant = c(0.95,0.75,0.5,0.25,0.05), 
   niter = exp(seq(log(ncol(X))*0.5, log(ncol(X)), length.out = 20)),
-  aggfun = "max", targsemu = 1, verbose = TRUE
+  estfun = "mean", aggfun = "max", rseTarget = 1, ci = 0.95, verbose = TRUE
 ){
 
   # add some tests of X dims
   
   # empty matrix for bootstrapped quantiles
-  Qn <- array(NaN, dim=c(nrow(X), length(quant), nboot),
+  qn <- array(NaN, dim=c(nrow(X), length(quant), nboot),
     dimnames = list(year=seq(nrow(X)), quant=quant, boot=seq(nboot)))
   # empty matrix for std.err and mean results
-  SE <- MU <- array(NaN, dim=c(nrow(X), length(quant), length(niter)),
+  est <- se <- cilow <- ciup <- array(NaN, dim=c(nrow(X), length(quant), length(niter)),
     dimnames = list(year=seq(nrow(X)), quant=quant, niter=niter))
   for(i in seq(niter)){
     for(n in seq(nboot)){
       if(nrow(X)==1){
-        Qn[,,n] <- quantile(X[,sample(ncol(X), niter[i], replace = TRUE)], prob=quant)
+        qn[,,n] <- quantile(X[,sample(ncol(X), niter[i], replace = TRUE)], prob=quant)
       } else {
-        Qn[,,n] <- t(apply(X[,sample(ncol(X), niter[i], replace = TRUE)], 
+        qn[,,n] <- t(apply(X[,sample(ncol(X), niter[i], replace = TRUE)], 
           MARGIN = 1, FUN = quantile, prob=quant))
       }
     }
-    MU[,,i] <- apply(Qn, MARGIN = 1:2, FUN = mean)
-    SE[,,i] <- apply(Qn, MARGIN = 1:2, FUN = sd)
+    est[,,i] <- apply(qn, MARGIN = 1:2, FUN = estfun)
+    se[,,i] <- apply(qn, MARGIN = 1:2, FUN = sd) # se of estimate
+    cilow[,,i] <- apply(qn, MARGIN = 1:2, FUN = quantile, prob = (1-ci)/2 )
+    ciup[,,i] <- apply(qn, MARGIN = 1:2, FUN = quantile, prob = (ci+(1-ci)/2) )
 
     if(verbose) print(paste0("niter = ", i, " of ", length(niter), " is finished"))
   }
   
-  SEMU <- SE/MU*100
+  rse <- se/est*100 # relative standard error 
   
   # fit log-log linear regression; lm(log(niter) ~ log(semu) + quant)
-  df <- expand.grid(dimnames(SEMU))
-  df$semu <- c(SEMU)
-  agg <- aggregate(semu ~ quant + niter, data = df, FUN = aggfun)
+  df <- expand.grid(dimnames(rse))
+  df$rse <- c(rse)
+  agg <- aggregate(rse ~ quant + niter, data = df, FUN = aggfun)
   agg$niter <- as.numeric(as.character(agg$niter))
-  fit <- lm(log(niter) ~ log(semu) + quant, agg)
-  newdat <- data.frame(semu = targsemu, quant = factor(quant))
+  fit <- lm(log(niter) ~ log(rse) + quant, agg)
+  newdat <- data.frame(rse = rseTarget, quant = factor(quant))
   newdat$niter <- exp(predict(fit, newdata = newdat))
   
   # return results
-  ret <- list(SE = SE, MU = MU, SEMU = SEMU, fit = fit, nsim = newdat,
+  ret <- list(est = est, se = se, cilow = cilow, ciup = ciup, rse = rse,
+    fit = fit, nsim = newdat,
     nboot = nboot, quant = quant, niter = niter, agg = agg,
-    aggfun = aggfun, targsemu = targsemu)
+    aggfun = aggfun, estfun = estfun, ci = ci, rseTarget = rseTarget)
   class(ret) <- "qboot"
   return(ret)
 }
--- R/qbootplot.R
+++ R/qbootplot.R
@@ -1,26 +1,43 @@
-
-
-qbootplot <- function(obj, pch = 1, col = NA, ...){
-  if(is.na(col)) col <- seq(obj$quant)
+#' Plot results of qboot
+#'
+#' @param obj 
+#' @param pch 
+#' @param col 
+#' @param ... 
+#'
+#' @return plot of qboot results
+#' @export
+#'
+#' @examples
+#' load("data/SSB.Rdata")
+#' source("R/qboot.R")
+#' source("R/qbootplot.R")
+#' X <- array(ssb, dim = dim(ssb)[c(2,6)])
+#' dimnames(X) <- dimnames(ssb)[c(2,6)]
+#' res <- qboot(X, nboot = 20, rseTarget = 1, aggfun = "max")
+#' qbootplot(res, pch = 1)
+#' 
+qbootplot <- function(obj, pch = 1, col = NULL, ...){
+  if(missing(col)) col <- seq(obj$quant)
   
   newdat <- expand.grid(
-    semu = seq(min(obj$agg$semu), max(obj$agg$semu), length.out = 100),
+    rse = seq(min(obj$agg$rse), max(obj$agg$rse), length.out = 100),
     quant = unique(obj$agg$quant))
   newdat$niter <- exp(predict(obj$fit, newdata = newdat)) 
   
-  plot(niter ~ semu, obj$agg, log = "xy", pch = pch, col = col, ...)
+  plot(niter ~ rse, obj$agg, log = "xy", pch = pch, col = col, ...)
   legend("topright", legend = paste(obj$quant, "=", round(obj$nsim$niter)), 
-    pch = pch, col = col)
+    pch = pch, col = col, ...)
 
   for(i in seq(levels(newdat$quant))){
-    lines(niter ~ semu, data = newdat, 
+    lines(niter ~ rse, data = newdat, 
       subset = quant == levels(newdat$quant)[i], 
-      col = col[i])
+      col = col[i], ...)
   }
-  abline(v = obj$targsemu, col = 8)
+  abline(v = obj$rseTarget, col = 8)
   for(i in seq(levels(newdat$quant))){
     hit <- which(factor(obj$nsim$quant) == levels(newdat$quant)[i])
-    segments(x0 = 1e-6, x1 = obj$targsemu, 
+    segments(x0 = 1e-6, x1 = obj$rseTarget, 
       y0 = obj$nsim$niter[hit], y1 = obj$nsim$niter[hit], 
       col = col[i], lty = 2)
   }
